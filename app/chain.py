d22301bfd842cbb134f2df509eba421e3e55afd1
federico Lubbert <federico.lubbert@gmail.com>
2024-05-02 10:18:04 +0200

    added project files

---
+++ app/chain.py
@@ -0,0 +1,60 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.embeddings import CohereEmbeddings
+from langchain_community.vectorstores import Pinecone
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from pinecone import Pinecone as PineconeClient
+import requests
+load_dotenv()
+
+# Keys
+PINECONE_API_KEY = os.environ["PINECONE_API_KEY"]
+PINECONE_ENVIRONMENT = os.environ["PINECONE_ENVIRONMENT"]
+PINECONE_INDEX_NAME = os.environ["PINECONE_INDEX_NAME"]
+
+pinecone = PineconeClient(api_key=PINECONE_API_KEY,
+                         environment=PINECONE_ENVIRONMENT)
+
+embeddings = CohereEmbeddings(model="multilingual-22-12")
+vectorstore = Pinecone.from_existing_index(index_name=PINECONE_INDEX_NAME,
+                                           embedding=embeddings)
+
+retriever = vectorstore.as_retriever()
+
+def fetch_wikipedia_page(id):
+    url = f"https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&pageids={id}"
+    response = requests.get(url)
+    data = response.json()
+    page_content = list(data['query']['pages'].values())[0]['extract']
+    return page_content
+
+def fetch_url(x):
+    urls = [doc.metadata['url'] for doc in x['context']]
+    ids = [url.split('=')[-1] for url in urls]
+    contents = [fetch_wikipedia_page(id)[:32000] for id in ids]    
+    return {"context": contents, "question": x["question"]}
+
+
+# RAG prompt
+template = """Answer the question based only on the following context:
+{context}
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+# RAG
+model = ChatOpenAI(temperature=0, model="gpt-4-1106-preview")
+
+chain = (
+    RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
+    | RunnableLambda(fetch_url)  # Add this line
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+
--- app/server.py
+++ app/server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
+from app.chain import chain as pinecone_wiki_chain
 
 app = FastAPI()
 
@@ -11,7 +12,7 @@ async def redirect_root_to_docs():
 
 
 # Edit this to add the chain you want to add
-add_routes(app, NotImplemented)
+add_routes(app, pinecone_wiki_chain, path="/pinecone-wikipedia")
 
 if __name__ == "__main__":
     import uvicorn
