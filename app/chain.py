c9b99a34ec46921ae390753cc5a1090f3dc2d9eb
federico Lubbert <federico.lubbert@gmail.com>
2024-05-03 22:28:13 +0200

    adjusting for different vectorstore

--- app/chain.py
+++ app/chain.py
@@ -1,5 +1,4 @@
 import os
-
 from dotenv import load_dotenv
 from langchain_community.chat_models import ChatOpenAI
 from langchain_community.embeddings import CohereEmbeddings
@@ -9,6 +8,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
 from pinecone import Pinecone as PineconeClient
 import requests
+
 load_dotenv()
 
 # Keys
@@ -17,7 +17,7 @@ PINECONE_ENVIRONMENT = os.environ["PINECONE_ENVIRONMENT"]
 PINECONE_INDEX_NAME = os.environ["PINECONE_INDEX_NAME"]
 
 pinecone = PineconeClient(api_key=PINECONE_API_KEY,
-                         environment=PINECONE_ENVIRONMENT)
+                          environment=PINECONE_ENVIRONMENT)
 
 embeddings = CohereEmbeddings(model="multilingual-22-12")
 vectorstore = Pinecone.from_existing_index(index_name=PINECONE_INDEX_NAME,
@@ -25,17 +25,10 @@ vectorstore = Pinecone.from_existing_index(index_name=PINECONE_INDEX_NAME,
 
 retriever = vectorstore.as_retriever()
 
-def fetch_wikipedia_page(id):
-    url = f"https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&pageids={id}"
-    response = requests.get(url)
-    data = response.json()
-    page_content = list(data['query']['pages'].values())[0]['extract']
-    return page_content
 
 def fetch_url(x):
-    urls = [doc.metadata['url'] for doc in x['context']]
-    ids = [url.split('=')[-1] for url in urls]
-    contents = [fetch_wikipedia_page(id)[:32000] for id in ids]    
+    # Retrieve the documents directly from the retriever's context
+    contents = [doc.page_content for doc in x['context']]
     return {"context": contents, "question": x["question"]}
 
 
@@ -50,11 +43,9 @@ prompt = ChatPromptTemplate.from_template(template)
 model = ChatOpenAI(temperature=0, model="gpt-4-1106-preview")
 
 chain = (
-    RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
-    | RunnableLambda(fetch_url)  # Add this line
-    | prompt
-    | model
-    | StrOutputParser()
+        RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
+        | RunnableLambda(fetch_url)  # Add this line
+        | prompt
+        | model
+        | StrOutputParser()
 )
-
-
