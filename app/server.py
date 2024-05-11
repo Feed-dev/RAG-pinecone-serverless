c12c7380c29c6d3d19b3a3508840134064ee7e17
federico Lubbert <federico.lubbert@gmail.com>
2024-05-11 08:13:36 +0200

    adjusting for different vectorstore

--- app/server.py
+++ app/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
-from app.chain import chain as pinecone_wiki_chain
+from app.chain import chain as pinecone_nordic_chain
 
 app = FastAPI()
 
@@ -12,9 +12,8 @@ async def redirect_root_to_docs():
 
 
 # Edit this to add the chain you want to add
-add_routes(app, pinecone_wiki_chain, path="/pinecone-wikipedia")
+add_routes(app, pinecone_nordic_chain)
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
