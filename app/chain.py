9d142c23cdc5b42839a936e38f6948dafedf87bf
federico Lubbert <federico.lubbert@gmail.com>
2024-05-14 14:11:29 +0200

    testing the rag

--- app/chain.py
+++ app/chain.py
@@ -62,5 +62,5 @@ def test_rag_chain(query):
 
 
 if __name__ == "__main__":
-    test_query = "How does one go to Valhalla?"
+    test_query = "What is the meaning of the runes?"
     test_rag_chain(test_query)
